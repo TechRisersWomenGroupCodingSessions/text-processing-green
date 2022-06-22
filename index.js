d460a54f1b4d69c67dc75a22340dbcd646893611
Martha Damian <martha.damian@eset.com>
2022-06-22 11:05:42 -0700

    added function to analyze text

---
+++ index.js
@@ -0,0 +1,11 @@
+function topTenList(textString) {
+    //parse words considering white space, commas, dashes, period (e.g. mid-day) into maybe a dictionary key = word value = word count
+    const wordsArray = textString.split(" ");
+    console.log(wordsArray);
+
+    //count words
+
+    //display numbered list
+}
+
+module.exports = topTenList; 
