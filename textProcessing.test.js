89fbecf9a83d6b5f7982abed13688b2c4ebe1169
Nicola Hearn <72850665+NIcolaHearn@users.noreply.github.com>
2022-07-20 19:29:53 +0100

    wrote new test line 32 and updated test line 39

--- textProcessing.test.js
+++ textProcessing.test.js
@@ -19,20 +19,27 @@ describe("index", () => {
 		expect(text.countWords(textString)).toBe(3);
 	});
 
-	it("creates an object of key value pairs of words and the number of times they appear in the text", () => {
+	// it("creates an object of key value pairs of words and the number of times they appear in the text", () => {
+	// 	const textString = "Hello, hello you!";
+	// 	console.log(text.wordOccurrences(textString))
+	// 	console.log(text.frequencies)
+	// 	expect(text.wordOccurrences(textString)).toStrictEqual({
+	// 		hello: 2,
+	// 		you: 1,
+	// 	});
+	// });
+
+	it("creates an array of all the words ordered by the number of occurences of each word", () => {
 		const textString = "Hello, hello you!";
-		console.log(text.wordOccurrences(textString))
-		console.log(text.frequencies)
-		expect(text.wordOccurrences(textString)).toStrictEqual({
-			hello: 2,
-			you: 1,
-		});
+		expect(text.wordOccurrences(textString)).toStrictEqual(
+			["hello", "you"]
+		);
 	});
 
-	it("evaluates the text and returns a list of the top 10 used words", () => {
-		const text = "Hello, hello you";
+	it("evaluates the text and returns a list of the top 5 used words", () => {
+		const text = "Hello, hello you you you are are are are very very very very very hot hot hot hot hot hot hot today";
 		
-		expect(topTenList(text)).toStrictEqual("1. hello\n 2. you");
+		expect(topTenList(text)).toStrictEqual(["hot", "very", "are", "you", "hello"]);
 	});
 });
 
